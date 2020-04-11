e242269e2ae8a7b1fe172f8775ebb79b8d90735c
mudathirdev <49608458+mudathirdev@users.noreply.github.com>
2020-04-11 17:49:30 +0200

    Update __manifest__.py

--- odoo_app_test2/__manifest__.py
+++ odoo_app_test2/__manifest__.py
@@ -21,13 +21,14 @@
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
-
+    'price':'109',
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
+    
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
